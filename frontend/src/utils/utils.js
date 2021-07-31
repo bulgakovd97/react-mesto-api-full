56c881e340297f657e946199672f707cb8db58c9
Denis Bulgakov <bulgakovd97@yandex.ru>
2021-07-31 16:52:11 +0300

    dev: the 15-th project

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,14 +1,12 @@
 export const options = {
-  // url: "http://api.mesto.bulgakovd.nomoredomains.club",
-  url: "http://localhost:3000",
+  url: "https://api.mesto.bulgakovd.nomoredomains.club",
   headers: {
     "Content-Type": "application/json",
   },
 };
 
 export const authOptions = {
-  // url: "http://api.mesto.bulgakovd.nomoredomains.club",
-  url: "http://localhost:3000",
+  url: "https://api.mesto.bulgakovd.nomoredomains.club",
   headers: {
     Accept: "application/json",
     "Content-Type": "application/json",
